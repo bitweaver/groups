1009aa416300a5ca07c30e683f43f2373e95b10b
wjames5 <will@tekimaki.com>
2008-02-07 03:20:46 +0000

    woops that was stupid. if group id is set we look up the group, if not then deliver a splash page about groups

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_groups/index.php,v 1.3 2008/02/07 02:38:11 wjames5 Exp $
+// $Header: /cvsroot/bitweaver/_bit_groups/index.php,v 1.4 2008/02/07 03:20:46 wjames5 Exp $
 // Copyright (c) 2004 bitweaver Group
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -10,10 +10,22 @@ require_once( '../bit_setup_inc.php' );
 // Is package installed and enabled
 $gBitSystem->verifyPackage( 'group' );
 
+if( !isset( $_REQUEST['group_id'] ) ) {
+	// if a default group has been set we'll use that
+	$_REQUEST['group_id'] = $gBitSystem->getConfig( "home_group" );
+}
+
 require_once( GROUP_PKG_PATH.'lookup_group_inc.php' );
 
+// Now check permissions to access this page
 $gContent->verifyViewPermission();
 
-// Display the template
-$gBitSystem->display( 'bitpackage:group/group_home.tpl', tra( 'Groups' ) );
+if( !isset( $_REQUEST['group_id'] ) || !$gContent->isValid() ) {
+	// if no group is requested, if no default is set, or the group requested is not valid we deliver a splash page about groups
+	$gBitSystem->display( 'bitpackage:group/group_home.tpl', tra( 'Groups' ) );
+}else{
+	$gContent->addHit();
+	// Display the template
+	$gBitSystem->display( 'bitpackage:group/group_display.tpl', tra( 'Group' ) );
+}
 ?>
