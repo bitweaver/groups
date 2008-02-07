926fd9462b49effd056c5fd4d6e27a0367c52c98
wjames5 <will@tekimaki.com>
2008-02-07 02:38:11 +0000

    create a splash page for groups as default home - eventually might want to make this an option in admin panel or a center magic tpl

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_groups/index.php,v 1.2 2008/02/04 19:09:10 nickpalmer Exp $
+// $Header: /cvsroot/bitweaver/_bit_groups/index.php,v 1.3 2008/02/07 02:38:11 wjames5 Exp $
 // Copyright (c) 2004 bitweaver Group
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -10,25 +10,10 @@ require_once( '../bit_setup_inc.php' );
 // Is package installed and enabled
 $gBitSystem->verifyPackage( 'group' );
 
-// Now check permissions to access this page
-/* DEPRECATED I THINK -wjames
-$gBitSystem->verifyPermission( 'p_group_read' );
-*/
-
-if( !isset( $_REQUEST['group_id'] ) ) {
-	$_REQUEST['group_id'] = $gBitSystem->getConfig( "home_group" );
-}
-
 require_once( GROUP_PKG_PATH.'lookup_group_inc.php' );
 
-if( !$gContent->isValid() ) {
-	$gBitSystem->setHttpStatus( 404 );
-	$gBitSystem->fatalError( "The group you requested could not be found." );
-}
-
 $gContent->verifyViewPermission();
-$gContent->addHit();
 
 // Display the template
-$gBitSystem->display( 'bitpackage:group/group_display.tpl', tra( 'Group' ) );
+$gBitSystem->display( 'bitpackage:group/group_home.tpl', tra( 'Groups' ) );
 ?>
---
+++ templates/group_home.tpl
@@ -0,0 +1,11 @@
+{strip}
+<div class="display group">
+	<div class="body">
+		<div class="content">
+			<p>@TODO insert block here displaying groups welcome message or if user is registered and has groups the list of groups they are a member and/or owner of.</p>
+			<br/><br/>
+			<p>@TODO insert here a list of existing groups and a search form for looking for groups</p>
+		</div><!-- end .content -->
+	</div><!-- end .body -->
+</div>
+{/strip}
