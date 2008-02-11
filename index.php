1f2869eb84beebb777bf00e4b32cfa3b79e5cc4e
wjames5 <will@tekimaki.com>
2008-02-11 00:53:02 +0000

    redirect to list if find is requested

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_groups/index.php,v 1.4 2008/02/07 03:20:46 wjames5 Exp $
+// $Header: /cvsroot/bitweaver/_bit_groups/index.php,v 1.5 2008/02/11 00:53:02 wjames5 Exp $
 // Copyright (c) 2004 bitweaver Group
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -9,6 +9,12 @@ require_once( '../bit_setup_inc.php' );
 
 // Is package installed and enabled
 $gBitSystem->verifyPackage( 'group' );
+
+//if the request is from the search field redirect us to the list - otherwise do what we do
+if ( isset( $_REQUEST['find'] ) ){
+	header ("location: ".GROUP_PKG_URI."list_groups.php?find=".$_REQUEST['find'] );
+	die;
+}
 
 if( !isset( $_REQUEST['group_id'] ) ) {
 	// if a default group has been set we'll use that
