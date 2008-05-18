b37c7f280354287d8725378dc919fc94e1e994ee
laetzer <laetzer@users.sourceforge.net>
2008-05-18 02:31:15 +0000

    replaced questions with statements; tried simpler explanation for difficult concept

--- options_inc.php
+++ options_inc.php
@@ -3,22 +3,22 @@
 $formGroupOptions = array(
 	"view_content_public" => array(
 		'label' => 'Publicly Viewable',
-		'note' => 'Is this group and its content publically viewable?',
+		'note' => 'When checked, this group and its content is publically viewable.',
 		'default' => 'y',
 	),
 	"is_public" => array(
 		'label' => 'Public Membership',
-		'note' => 'Is anyone free to join this group? Unchecking this box means an invitation will be required to join this group',
+		'note' => 'When checked, anyone is free to join this group. When not checked, an invitation will be required to join this group.',
 		'default' => 'y',
 	),
 	"mod_msgs" => array(
 		'label' => 'Moderate Messages',
-		'note' => 'When checked all messages to this group will be held for moderation before being displayed.',
+		'note' => 'When checked, all messages to this group will be held for moderation before being displayed.',
 		'default' => 'n',
 	),
 	"mod_content" => array(
 		'label' => 'Moderate Content',
-		'note' => 'When checked any content created (like pages or blog posts) wiil be held for moderation before being displayed',
+		'note' => 'When checked, all content created (like pages or blog posts) will be held for moderation before being displayed',
 		'default' => 'n',
 	),
 );
@@ -26,7 +26,7 @@ $formGroupOptions = array(
 if ( $gBitSystem->isFeatureActive( 'group_admin_content' ) ){
 	$formGroupOptions['admin_content_strict'] = array(
 		'label' => 'Assert Administrative Control of Content',
-		'note' => 'Can the group administrator administrate any content in the group? This means being able to edit any of the content, vs. standard control of allowing it be linked to the group.',
+		'note' => 'When checked, only the group administrator can control content in the group. When not checked, members are allowed to assign content to the group.',
 		'default' => 'n',
 	);
 }
