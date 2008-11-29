50209366815f7ee9b0f39bc1bd8e3093695a83d5
Tekimaki <tekimaki_admin@users.sourceforge.net>
2008-11-29 11:33:08 +0000

    Add support for pulling groups with a friendly name.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_groups/index.php,v 1.26 2008/06/18 13:18:20 lsces Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_groups/index.php,v 1.27 2008/11/29 11:33:08 tekimaki_admin Exp $
  * Copyright (c) 2008 bitweaver Group
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -18,6 +18,15 @@ require_once( '../bit_setup_inc.php' );
 if ( isset( $_REQUEST['find'] ) ){
 	header ("location: ".GROUP_PKG_URI."list_groups.php?find=".$_REQUEST['find'] );
 	die;
+}
+
+// Lookup by name?
+if( !empty( $_REQUEST['group_name'] ) ) {
+	global $gBitDb;
+	$_REQUEST['group_id'] = $gBitDb->getOne( "SELECT group_id FROM `".BIT_DB_PREFIX."groups` g LEFT JOIN `".BIT_DB_PREFIX."liberty_content` lc ON (g.`content_id` = lc.`content_id`) WHERE lc.`title` = ?", array($_REQUEST['group_name']) );
+	if( empty( $_REQUEST['group_id'] ) ) {
+	  $gBitSystem->fatalError(tra('No group found with the name: ').$_REQUEST['group_name']);
+	}
 }
 
 if( !isset( $_REQUEST['group_id'] ) ) {
