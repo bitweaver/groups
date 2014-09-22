0d61d5db959cb939361040c1f95e778b1a8cd66b
spiderr <spider@viovio.com>
2014-09-22 18:05:44 -0400

    getTitleFromHash-cleanup

--- BitGroup.php
+++ BitGroup.php
@@ -629,7 +629,7 @@ class BitGroup extends LibertyMime {
 		return $ret;
 	}
 
-	function getTitle( $pParamHash = NULL, $pContentId = NULL ){
+	function getTitleFromHash( $pParamHash = NULL, $pContentId = NULL ){
 		$ret = NULL;
 
 		if( @BitBase::verifyId( $pContentId ) ){
